39098b5dd8f7e3b2e5d1bdd82d6603f8a2cf08e4
Jordan M <jordan.muliaman@gmail.com>
2020-05-28 20:53:58 -0700

    add handlebars

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
 app.use(require('./routes'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 app.listen(3000, () => console.log('http://localhost:3000'))
